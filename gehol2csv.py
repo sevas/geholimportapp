983907018f2dfa1dfef2764aee371b62e5bd7e78
odebeir <none@none>
2010-12-15 17:35:49 +0100

    remove temporary ical dependencies

--- gehol2csv.py
+++ gehol2csv.py
@@ -1,6 +1,6 @@
 from gehol.coursecalendar import CourseCalendar
 from gehol.csvwriter import to_csv
-from gehol.icalwriter import export_ical
+#from gehol.icalwriter import export_ical
 
 
 host = '164.15.72.157:8080'
@@ -10,10 +10,10 @@ def gehol2csv(course):
     cal = CourseCalendar(host, course)
     cal.load_events()
     csv_string = to_csv(cal.metadata, cal.events, first_monday)
-    ical_string = export_ical(cal.metadata, cal.events, first_monday)
+#    ical_string = export_ical(cal.metadata, cal.events, first_monday)
 ##    return csv_string
 #    print csv_string
-    return [csv_string,ical_string]
+    return [csv_string,'no ical yet']
 
 #    cal = CourseCalendar(host, course)
 #    cal.load_events()
