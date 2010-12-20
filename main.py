f74a3466d450652109778cd6fc73537d6ca3b2e3
sevas <f.degroef@gmail.com>
2010-12-20 20:47:31 +0100

    added a new type of page : coursecalendar. presents links to ics/csv files

--- main.py
+++ main.py
@@ -31,9 +31,6 @@ class MainPage(webapp.RequestHandler):
 
 class Calendar(webapp.RequestHandler):
     def post(self):
-
-        request = PreviousRequest()
-
         course_mnemo = self.request.get('course_mnemo')
 
         cal = get_calendar(course_mnemo)
@@ -42,6 +39,8 @@ class Calendar(webapp.RequestHandler):
         events_csv = csv.splitlines()
         events_ical = ical.splitlines()
 
+
+        request = PreviousRequest()
         #if success, the request is recorded
         if not error:
             request.content = self.request.get('course_mnemo')
@@ -50,9 +49,9 @@ class Calendar(webapp.RequestHandler):
         template_values = {
             'mnemo':course_mnemo,
             'calendar':cal,
-            'events_csv': events_csv,
+            'events_csv@': events_csv,
             'events_ical': events_ical,
-            }
+        }
 
         template_values.update(cal.metadata)
         template_values.update({'gehol_is_down' : is_status_down(),
@@ -63,25 +62,63 @@ class Calendar(webapp.RequestHandler):
 
 
 
+class CourseCalendar(webapp.RequestHandler):
+    def get(self):
+        parsed = urlparse.urlparse(self.request.uri)
+        course_mnemo = self._get_course_mnemo(parsed.path)
+
+        cal = get_calendar(course_mnemo)
+        ical_url, csv_url = self._build_file_urls(course_mnemo)
+
+        template_values = {'gehol_is_down': is_status_down(),
+                           'last_status_update': get_last_status_update(),
+                           'mnemo':course_mnemo,
+                           'ical_url':ical_url,
+                           'csv_url':csv_url
+        }  
+
+        template_values.update(cal.metadata)
+
+        path = os.path.join(os.path.dirname(__file__), 'templates/course.html')
+        self.response.out.write(template.render(path, template_values))
+
+
+    @staticmethod
+    def _build_file_urls(course_mnemo):
+        return ("/ical/%s.ics" % course_mnemo,
+                "/csv/%s.csv" % course_mnemo)
+
+    @staticmethod
+    def _get_course_mnemo(path):
+        return path.split('/')[2]
+
+
+
 class IcalRenderer(webapp.RequestHandler):
     def get(self):
-
-        self.response.headers['Content-Type'] = 'text/plain'
         parsed = urlparse.urlparse(self.request.uri)
         course_mnemo = parsed.path.split("/")[2].rstrip(".ics")
 
-        cal = get_calendar(course_mnemo)
+        if self._is_course_mnemo_valid(course_mnemo):
+            cal = get_calendar(course_mnemo)
 
-        error,csv,ical = convert_calendar(cal)
-        #events_csv = csv.splitlines()
-        events_ical = ical#.splitlines()
+            error,csv,ical = convert_calendar(cal)
+            events_ical = ical
 
-        self.response.out.write(events_ical)
+            self.response.headers['Content-Type'] = "text/plain;  charset=utf-8"
+            self.response.out.write(events_ical)
+        else:
+            self.redirect("/ical/notfound")
+        
+    @staticmethod
+    def _is_course_mnemo_valid(course_mnemo):
+        return course_mnemo.isalnum() and len(course_mnemo) == 8
 
 
 application = webapp.WSGIApplication(
                                      [('/', MainPage),
                                      ('/cal', Calendar),
+                                     ('/course/.*', CourseCalendar),
                                      ('/ical/.*\.ics', IcalRenderer),
                                      ('/geholstatus',  UpdateGeholStatus),
                                      ],
