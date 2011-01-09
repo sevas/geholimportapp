d743e7cb4cf9ebb0992695c8ba31c20c9bc51857
sevas <f.degroef@gmail.com>
2011-01-09 23:53:10 +0100

    updated gehol status check cron job : test for the gateway AND the scientia backend

--- check_status.py
+++ check_status.py
@@ -4,6 +4,29 @@ import logging
 from google.appengine.ext import webapp
 from status import set_status_down
 
+
+def check_server(host):
+    headers = {"Content-type": "application/x-www-form-urlencoded",
+               "Accept": "text/plain"}
+
+    conn = httplib.HTTPConnection(host)
+    conn.request("GET", '/', headers = headers)
+    response = conn.getresponse()
+    return True
+
+
+def check_gateway():
+    host = "164.15.72.157"    
+    logging.info("Testing GeHoL gateway : %s " % host)
+    return check_server(host)
+
+def check_scientia_backend():
+    host = "164.15.72.157:8080"    
+    logging.info("Testing Scientia backend : %s " % host)
+    return check_server(host)
+
+
+
 class UpdateGeholStatus(webapp.RequestHandler):
     def get(self):
         logging.info("Launching UpdateGeholStatus cron job")
@@ -11,13 +34,9 @@ class UpdateGeholStatus(webapp.RequestHandler):
 
 
     def update_gehol_status(self):
-        host = "164.15.72.157:8080"
-        headers = {"Content-type": "application/x-www-form-urlencoded",
-                   "Accept": "text/plain"}
         try:
-            conn = httplib.HTTPConnection(host)
-            conn.request("GET", '/', headers = headers)
-            response = conn.getresponse()
+            check_gateway()
+            check_scientia_backend()
             set_status_down(False)
         except Exception,e:
             set_status_down(True)
